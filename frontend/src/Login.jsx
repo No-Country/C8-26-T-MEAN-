fd23d0083f1f98e2b43cc048f7f07f2f97feda8d
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-11-15 15:15:28 -0500

    Se eliminó el código relacionado a los íconos de otras redes sociales.

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -11,23 +11,6 @@ function Login() { //los componentes en React son funciones. Su nombre comienza 
             <div className="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
               <div className="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
                 <h4 className="text-white font-weight-bolder text-center mt-2 mb-0">Iniciar sesión</h4>
-                <div className="row mt-3">
-                  <div className="col-2 text-center ms-auto">
-                    <a className="btn btn-link px-3" href="javascript:;">
-                      <i className="fa fa-facebook text-white text-lg"></i>
-                    </a>
-                  </div>
-                  <div className="col-2 text-center px-1">
-                    <a className="btn btn-link px-3" href="javascript:;">
-                      <i className="fa fa-github text-white text-lg"></i>
-                    </a>
-                  </div>
-                  <div className="col-2 text-center me-auto">
-                    <a className="btn btn-link px-3" href="javascript:;">
-                      <i className="fa fa-google text-white text-lg"></i>
-                    </a>
-                  </div>
-                </div>
               </div>
             </div>
             <div className="card-body">
