51a338b41ea37d1270cb5f100b2124dc478a50d0
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-11-15 13:56:56 -0500

    Añadí el componente Login, el contenido es igual al de App.jsx. Aún no tiene estilos o algo por el estilo.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client' //para renderizar
 import App from './App'
+import Login from './Login'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render( //se selecciona el elemento con ID root del HTML
   <React.StrictMode> {/* el modo estricto de JS permite hacer comprobaciones. El de React es un poco mejor. Sirve para debugear, para el desarrollo.*/}
     <App /> {/*componente de React. Se rederiza App.jsx*/}
+    <Login />
   </React.StrictMode>
 )
