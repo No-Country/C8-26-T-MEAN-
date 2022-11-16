e10d4e06137b3ecd301191005e56462bd74532ae
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-11-15 21:32:52 -0500

    Se eliminó el experimento que hice relacionado al login. No se veía como debería.

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,40 +1,10 @@
-import { useState } from 'react'
+import React from 'react'
 import './styles/index.css'
 
 function Login() { //los componentes en React son funciones. Su nombre comienza en mayúscula. Retorna jsx
   return (
     <div className="Login">
-      <div className="container my-auto">
-      <div className="row">
-        <div className="col-lg-4 col-md-8 col-12 mx-auto">
-          <div className="card z-index-0 fadeIn3 fadeInBottom">
-            <div className="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-              <div className="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
-                <h4 className="text-white font-weight-bolder text-center mt-2 mb-0">Iniciar sesión</h4>
-              </div>
-            </div>
-            <div className="card-body">
-              <form role="form" className="text-start">
-                <div className="input-group input-group-outline my-3">
-                  <label className="form-label">Correo electrónico o usuario</label>
-                  <input type="email" className="form-control"></input>
-                </div>
-                <div className="input-group input-group-outline mb-3">
-                  <label className="form-label">Contraseña</label>
-                  <input type="password" className="form-control"></input>
-                </div>
-                <div className="text-center">
-                  <button type="button" className="btn bg-gradient-primary w-100 my-4 mb-2">¡Ingresar!</button>
-                </div>
-                <p className="mt-4 text-sm text-center">
-                  Olvidé mi usuario o contraseña. :(
-                </p>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+      
     </div>
   )
 }
