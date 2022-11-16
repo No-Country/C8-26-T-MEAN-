f7fe1588eedc62c4b2a757522495b018c33b0a1f
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-11-16 00:00:29 -0500

    Se añadió un login semejante a lo que envió Emilce.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+//Este es el archivo principal que nos permite renderizar el proyecto en el navegador.
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+import React from 'react'
+import ReactDOM from 'react-dom/client' //para renderizar
+import App from './App'
+import Login from './Login'
+import './styles/index.css'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+ReactDOM.createRoot(document.getElementById('root')).render( //se selecciona el elemento con ID root del HTML
+  <React.StrictMode> {/* el modo estricto de JS permite hacer comprobaciones. El de React es un poco mejor. Sirve para debugear, para el desarrollo.*/}
+    {/*<App /> */} 
+    <Login />{/*componente de React. Se rederiza Login.jsx*/}
   </React.StrictMode>
 )
