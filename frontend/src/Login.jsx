28f282d357cfb54c05c70728bf14f166b447b079
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-11-15 22:29:00 -0500

    Cambios en el código del login. Es más cercano a lo que diseño Emilce. (:

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './styles/Login.css'
 import {
   MDBBtn,
   MDBContainer,
@@ -20,47 +19,24 @@ function Login() { //los componentes en React son funciones. Su nombre comienza 
         <MDBRow>
           <MDBCol md='6' className='text-center text-md-start d-flex flex-column justify-content-center'>
             <h1 className="my-5 display-3 fw-bold ls-tight px-3">
-              The best offer <br />
-              <span className="text-primary">for your business</span>
+               ¡Bienvenido a<br />
+              <span className="text-primary">GIFTClub!</span>
             </h1>
-            <p className='px-3' style={{ color: 'hsl(217, 10%, 50.8%)' }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Eveniet, itaque accusantium odio, soluta, corrupti aliquam
-              quibusdam tempora at cupiditate quis eum maiores libero
-              veritatis? Dicta facilis sint aliquid ipsum atque?
+            <p className='px-3'>
+              ¿Qué es GiftClub? GiftClub es un sistema de puntos acumulables que pueden ser canjeados
+              por cualquiera de los productos que se encuentran dentro del catálogo de este sitio. 
+              Recuerda que a más puntos más posibilidades de llevarte todos los productos. (;
+              <br />¡Que los disfrutes!
             </p>
           </MDBCol>
           <MDBCol md='6'>
             <MDBCard className='my-5'>
               <MDBCardBody className='p-5'>
-                <MDBRow>
-                  <MDBCol col='6'>
-                    <MDBInput wrapperClass='mb-4' label='First name' id='form1' type='text' />
-                  </MDBCol>
-                  <MDBCol col='6'>
-                    <MDBInput wrapperClass='mb-4' label='Last name' id='form1' type='text' />
-                  </MDBCol>
-                </MDBRow>
-                <MDBInput wrapperClass='mb-4' label='Email' id='form1' type='email' />
-                <MDBInput wrapperClass='mb-4' label='Password' id='form1' type='password' />
-                <div className='d-flex justify-content-center mb-4'>
-                  <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Subscribe to our newsletter' />
-                </div>
-                <MDBBtn className='w-100 mb-4' size='md'>sign up</MDBBtn>
+                <MDBInput wrapperClass='mb-4' label='Correo electrónico' id='form1' type='email' />
+                <MDBInput wrapperClass='mb-4' label='Contraseña' id='form1' type='password' />
+                <MDBBtn className='w-100 mb-4' size='md'>Ingresar</MDBBtn>
                 <div className="text-center">
-                  <p>or sign up with:</p>
-                  <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                    <MDBIcon fab icon='facebook-f' size="sm" />
-                  </MDBBtn>
-                  <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                    <MDBIcon fab icon='twitter' size="sm" />
-                  </MDBBtn>
-                  <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                    <MDBIcon fab icon='google' size="sm" />
-                  </MDBBtn>
-                  <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                    <MDBIcon fab icon='github' size="sm" />
-                  </MDBBtn>
+                  <p>Olvidé mi usuario o contraseña. :(</p>
                 </div>
               </MDBCardBody>
             </MDBCard>
