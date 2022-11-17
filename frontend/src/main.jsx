8a85f45005f681fdc87d44e522daf9af6f95a70e
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-11-17 00:10:22 -0500

    REFACTOR: Se eliminó código que no se utiliza.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client' //para renderizar
 import { HashRouter } from 'react-router-dom'
 import App from './App'
-import Login from './components/Login'
-import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render( //se selecciona el elemento con ID root del HTML
   <React.StrictMode> {/* el modo estricto de JS permite hacer comprobaciones. El de React es un poco mejor. Sirve para debugear, para el desarrollo.*/}
