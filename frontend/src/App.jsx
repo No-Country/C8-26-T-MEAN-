522268b980521bd23f78503682e39149b31eee7e
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-11-18 00:10:11 -0500

    Se añadiço el componente "Categories" a las rutas. Se puede acceder a la misma a través del puerto del localhost/#/Categories

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './components/Home'
 import Gift from './components/Gift'
 import Faq from './components/Faq'
 import Products from './components/Products'
+import Categories from './components/Categories'
 
 function App() { //los componentes en React son funciones. Su nombre comienza en mayúscula. Retorna jsx
 
@@ -10,6 +11,7 @@ function App() { //los componentes en React son funciones. Su nombre comienza en
     <div className="App">
      <Routes>
           <Route path='/' element={<Home/>} />
+          <Route path='/Categories' element={<Categories/>} />
           <Route path='/Gift' element={<Gift/>} />
           <Route path='/Faq' element={<Faq/>} />
           <Route path='/Products' element={<Products/>} />
