8e19fc5a16c09ccb249c387dcae1e7733bc8c844
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-11-17 22:38:07 -0500

    Se añadieron los cambios de la rama dev. (nuevos componentes: "Categories,jsx" y "Products.jsx")

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import {Route, Routes} from 'react-router-dom'
 import Home from './components/Home'
 import Gift from './components/Gift'
 import Faq from './components/Faq'
+import Products from './components/Products'
+import Categories from './components/Categories'
 
 function App() { //los componentes en React son funciones. Su nombre comienza en mayúscula. Retorna jsx
 
@@ -9,8 +11,10 @@ function App() { //los componentes en React son funciones. Su nombre comienza en
     <div className="App">
      <Routes>
           <Route path='/' element={<Home/>} />
+          <Route path='/Categories' element={<Categories/>} />
           <Route path='/Gift' element={<Gift/>} />
           <Route path='/Faq' element={<Faq/>} />
+          <Route path='/Products' element={<Products/>} />
      </Routes>
     </div>
   )
---
+++ frontend/src/components/Products.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Products = () => {
+    const [products, setProducts] = useState(null)
+     useEffect(() => {
+        const url ="http://localhost:3000/products"
+         axios.get(url)
+         .then( res => setProducts(res.data))
+         .catch(e=>console.log(e, "entro"))
+     }, [])
+     
+      console.log(products)
+  return (
+      <section  className='Products'>
+
+          <h1>Products</h1>
+      </section>
+
+  )
+}
+
+export default Products
