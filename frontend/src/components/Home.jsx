91bd401fadb63905835187fff4f0ff2fa090484f
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-11-19 22:58:02 -0500

    Se eliminó código innecesario.

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import Login from './Login'
 import Navbar from './Navbar'
 import CarouselOfi from './CarouselOfi'
-import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
-import '../styles/gift.css'
-
 
 const Home = () => {
   return (
