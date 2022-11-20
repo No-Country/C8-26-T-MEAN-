87931497eac3725acf491246a1d7988571b79842
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-11-20 00:15:32 -0500

    Elimnación de código innecesario.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,24 +2,21 @@ import {Route, Routes} from 'react-router-dom'
 import Home from './components/Home'
 import Gift from './components/Gift'
 import Faq from './components/Faq'
+import Login from './components/Login'
 import Products from './components/Products'
-
 import Categories from './components/Categories'
-
 
 function App() { //los componentes en React son funciones. Su nombre comienza en mayúscula. Retorna jsx
 
   return (
     <div className="App">
-      <Routes>
+     <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/Categories' element={<Categories/>} />
           <Route path='/Gift' element={<Gift/>} />
           <Route path='/Faq' element={<Faq/>} />
-          <Route path='/Products' element={<Products/>}/>
-          
-      </Routes>
-
+          <Route path='/Products' element={<Products/>} />
+     </Routes>
     </div>
   )
 }
