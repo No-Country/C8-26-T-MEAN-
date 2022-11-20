84d8f3225cc3239305e252de6fca767161480d2c
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-11-20 00:19:10 -0500

    REFACTOR: Elimnación de código innecesario.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,8 +1,7 @@
-import React, {useState} from 'react'
-
+import React from 'react'
+import axios from 'axios
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import axios from 'axios'
 import {
   MDBBtn,
   MDBContainer,
@@ -10,9 +9,7 @@ import {
   MDBCol,
   MDBCard,
   MDBCardBody,
-  MDBInput,
-  MDBCheckbox,
-  MDBIcon
+  MDBInput
 }
   from 'mdb-react-ui-kit';
 
@@ -29,7 +26,7 @@ function Login() { //los componentes en React son funciones. Su nombre comienza 
       console.log(e);
     })
   }
-  
+
   return (
     <div className="Login">
       <MDBContainer fluid className='p-4'>
@@ -37,11 +34,11 @@ function Login() { //los componentes en React son funciones. Su nombre comienza 
           <MDBCol md='6' className='text-center text-md-start d-flex flex-column justify-content-center'>
             <h1 className="my-5 display-3 fw-bold ls-tight px-3">
               ¡Bienvenido a<br />
-              <span className="text-primary">GIFTClub!</span>
+              <span className="text-primary" >GIFTClub!</span>
             </h1>
             <p className='px-3'>
-              ¿Qué es GiftClub? GiftClub es un sistema de puntos acumulables que pueden ser canjeados
-              por cualquiera de los productos que se encuentran dentro del catálogo de este sitio. 
+              ¿Qué es GiftClub? <br />GiftClub es un sistema de puntos acumulables que pueden ser canjeados
+              por cualquiera de los productos que se encuentran dentro del catálogo de este sitio.
               Recuerda que a más puntos más posibilidades de llevarte todos los productos. (;
               <br />¡Que los disfrutes!
             </p>
@@ -49,19 +46,11 @@ function Login() { //los componentes en React son funciones. Su nombre comienza 
           <MDBCol md='6'>
             <MDBCard className='my-5'>
               <MDBCardBody className='p-5'>
-                <MDBInput 
-                wrapperClass='mb-4' 
-                label='Correo electrónico' 
-                id='form1' 
-                type='email' />
+                <MDBInput wrapperClass='mb-4' label='Correo electrónico' id='form1' type='email' />
                 <MDBInput wrapperClass='mb-4' label='Contraseña' id='form1' type='password' />
-                <MDBBtn 
-                className='w-100 mb-4' 
-                size='md'
-                onClick={onSubmit}
-                >Ingresar</MDBBtn>
+                <MDBBtn className='w-100 mb-4' size='md' onClick={onSubmit}>Ingresar</MDBBtn>
                 <div className="text-center">
-                  <p>Olvidé mi usuario o contraseña. :(</p>
+                  <p>¿Olvidó su contraseña?</p>
                 </div>
               </MDBCardBody>
             </MDBCard>
