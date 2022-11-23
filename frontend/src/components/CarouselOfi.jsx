9aa83c6bec35f869c65b87aa76063d4dee6e66de
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-11-23 01:21:52 -0500

    Se cambió la imagen del carrusel.

--- frontend/src/components/CarouselOfi.jsx
+++ frontend/src/components/CarouselOfi.jsx
@@ -10,31 +10,24 @@ export default function CarouselOfi() {
       <MDBCarouselItem
         className='w-100 d-block'
         itemId={1}
-        src="https://mdbootstrap.com/img/Photos/Slides/img%20(35).jpg" /*1320px 583px*/
+        src="https://ik.imagekit.io/tjlqeq3ql/GiftClub/ban_espcialMundial2_KgT4WfoeT?ik-sdk-version=javascript-1.4.3&updatedAt=1669184322727" /*1320px 583px*/
         alt="copa fifa"
       >
-        <h5>TODO PREPARADO PARA LA COPA MUNDIAL DE LA FIFA CATAR 2022.</h5>
-        <p>Los esquipos estan preparados. Los fans ya estan en la calle y se siente la emocion, felicidad y el apoyo a cada país. </p>
       </MDBCarouselItem>
       <MDBCarouselItem
         className='w-100 d-block'
         itemId={2}
-        src='https://mdbootstrap.com/img/Photos/Slides/img%20(35).jpg'
+        src='https://ik.imagekit.io/tjlqeq3ql/GiftClub/ban_espcialMundial2_KgT4WfoeT?ik-sdk-version=javascript-1.4.3&updatedAt=1669184322727'
         alt='...'
       >
-        <h5>Acumula puntos y segui ganando!</h5>
-        <p>Descubre todas las alternativas para acumular puntos que te entrega Gift Club. 	
-           Cuando disfrutás de los beneficios y pagás con tus tarjetas, también cumplís desafíos y sumás puntos.</p>
       </MDBCarouselItem>
 
       <MDBCarouselItem
         className='w-100 d-block'
         itemId={3}
-        src='https://mdbootstrap.com/img/Photos/Slides/img%20(35).jpg'
+        src='https://ik.imagekit.io/tjlqeq3ql/GiftClub/ban_espcialMundial2_KgT4WfoeT?ik-sdk-version=javascript-1.4.3&updatedAt=1669184322727'
         alt='...'
       >
-        <h5>Messi te queremos</h5>
-        <p>VAMOS ARGENTINA</p>
       </MDBCarouselItem>
     </MDBCarousel>
   );
