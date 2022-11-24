bc100d8f52e5ac5727acc64a3318608de99a379c
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-11-24 18:56:43 -0500

    REFACTOR: Se añadieron elementos y se modificaron los estilos.

--- frontend/src/components/DetailProduct.jsx
+++ frontend/src/components/DetailProduct.jsx
@@ -1,24 +1,30 @@
+import React, { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import axios from 'axios'
 import Navbar from './Navbar'
 import DetailCard from './DetailCard'
-import axios from 'axios'
-import React,{useState,useEffect} from 'react'
-import { useParams } from 'react-router-dom'
+import '../styles/detailProduct.css'
+import { MDBBtn } from 'mdb-react-ui-kit';
+
 const DetailProduct = () => {
-    const {id} =useParams()
-    const [product, setProduct] = useState({})
-    useEffect(() => {
-       const url =`http://localhost:3001/products/${id}/`
-       axios.get(url)
-       .then(res =>setProduct(res.data.data))
-       .catch(e=>console.log(e))
-    }, [])
-    console.log(product)
+  const { id } = useParams()
+  const [product, setProduct] = useState({})
+  useEffect(() => {
+    const url = `http://localhost:3001/products/${id}/`
+    axios.get(url)
+      .then(res => setProduct(res.data.data))
+      .catch(e => console.log(e))
+  }, [])
+  console.log(product)
   return (
     <div>
-       <Navbar />
-        <DetailCard  product={product}/>
-    </div>
-   
+      <Navbar />
+      <Link to="/Categories">
+        <MDBBtn rounded className='btn-categ-prod' color='dark'>CATEGORÍAS</MDBBtn>
+      </Link>
+      <DetailCard product={product} />
+    </div >
+
   )
 }
 
