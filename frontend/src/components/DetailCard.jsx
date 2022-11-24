f3fb17aa9b27854becc990f69a822868672c7bff
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-11-24 18:08:01 -0500

    Se agregó el botón para que muestre el detalle de pedido.

--- frontend/src/components/DetailCard.jsx
+++ frontend/src/components/DetailCard.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/detailProduct.css'
-const DetailCard = ({product}) => {
+import { MDBBtn } from 'mdb-react-ui-kit';
+
+const DetailCard = ({ product }) => {
 
   return (
     <div className='detail--product'>
-          <div className='detail--product-img'>
-             <img
-                src={product.image_url}
-                alt=''
-                />
-          </div>
-         <div className='detail--product-text'>
-            <h3>{product.product_name} </h3>
-            <p>{product.short_description}</p>
-            <h4>{product.price}</h4>
-            <Link  to ="/Purchase">
-            Canjear
-            </Link>
-         </div>
+      <div className='detail--product-img'>
+        <img
+          src={product.image_url}
+          alt=''
+        />
+      </div>
+      <div className='detail--product-text'>
+        <h3 className='tex-product'>{product.product_name} </h3>
+        <p>{product.short_description} <br />Descripción general: {product.long_description} <br />Lo puedes canjear por: {product.price} puntos.
+        </p>
+        <Link to="/Purchase">
+          <MDBBtn rounded className='mx-2' color='secondary'>CANJEAR</MDBBtn>
+        </Link>
+      </div>
     </div>
   )
 }
