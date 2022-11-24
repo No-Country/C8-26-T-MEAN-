8140356366de7d6de784c36ba028be0ce8dc6b31
pgianferro <pablogianferro@gmail.com>
2022-11-24 00:55:42 -0300

    PG: arreglo login front

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+// import { Redirect } from 'react-router-dom';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import {
@@ -20,22 +21,21 @@ function Login(){
 
 	const username = useRef();
 	const password = useRef();
+	console.log(username);
 	const [loginError, setLoginError] = useState(false);
 	const [redirect, setRedirect] = useState(false);
 	
 	const loginFetch = (e) => {
 		e.preventDefault();
 		setLoginError(false);
+
 		
 		const cryptedPassword = crypto.AES.encrypt(password.current.value, privateSeed).toString();
-
-
-		const hashPassword = crypto.AES.decrypt(cryptedPassword, privateSeed);
-		const decryptedPassword = hashPassword.toString(crypto.enc.Utf8); 
-
+/*		const hashPassword = crypto.AES.decrypt(cryptedPassword, privateSeed);
+		const decryptedPassword = hashPassword.toString(crypto.enc.Utf8); */
 		const data = {
 			username: username.current.value,
-			password: cryptedPassword
+			password: cryptedPassword,
 		}
 
 		const registerLogin = data => {
@@ -61,38 +61,28 @@ function Login(){
 			.then(data => registerLogin(data));
 
 	}
-
-	return(
-			<div className="Login">
-			<MDBContainer fluid className='p-4'>
-			<MDBRow>
-				<MDBCol md='6' className='text-center text-md-start d-flex flex-column justify-content-center'>
-				<h1 className="my-5 display-3 fw-bold ls-tight px-3">
-					¡Bienvenido a<br />
-					<span className="text-primary" class="gif-club">GIFTClub!</span>
-				</h1>
 	
-				<h3>¿Qué es GiftClub?</h3>
-				<p className='px-3'>
-					GiftClub es un sistema de puntos acumulables que pueden ser canjeados
-					por cualquiera de los productos que se encuentran dentro del catálogo de este sitio. 
-					Recuerda que a más puntos más posibilidades de llevarte todos los productos.¡Que los disfrutes!
-				</p>
-				</MDBCol>
-				<MDBCol md='6'>
-				<MDBCard className='my-5'>
-					<MDBCardBody className='p-5'>
-					<MDBInput wrapperClass='mb-4' label='Correo electrónico' id='email' type='email'ref={username} />
-					<MDBInput wrapperClass='mb-4' label='Contraseña' id='password' type='password' ref={password} />
-					<MDBBtn className='w-100 mb-4' size='md' onClick={loginFetch}>Ingresar</MDBBtn>
-					<div className="text-center">
-						<p>¿Olvidó su contraseña?</p>
+	return(
+		<div className="container-fluid">
+				<>
+					<div className="row my-4">
+						<div className="col-12 col-md-6">
+							{/* Login Form */}
+							{/* <form method="POST" onSubmit={loginFetch}> */}
+								<div className="form-group">
+									<label htmlFor="">Nombre de usuario o email:</label>
+									<input type="text" ref={username} className="form-control" id="email"/>
+									<label htmlFor="">Contraseña:</label>
+									<input type="password" ref={password} className="form-control" id="password"/>
+								</div>
+									<button className="btn btn-info" onClick={loginFetch}>Login</button>
+									{redirect ? <redirect to="/"/> : ''}
+							{/* </form> */}
+						</div>
 					</div>
-					</MDBCardBody>
-				</MDBCard>
-				</MDBCol>
-			</MDBRow>
-			</MDBContainer>
+				</>
+			{loginError ? <div className="alert alert-danger text-center my-4 fs-2">Usuario o contraseña incorrecta</div> : <></>}
+			
 		</div>
 	)
 }
