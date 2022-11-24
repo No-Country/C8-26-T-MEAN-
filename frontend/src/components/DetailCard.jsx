0d1680f1c020c0bb7eb9bcf2c6da64c53df9f579
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-11-24 18:20:08 -0500

    REFACTOR: Se modificaron los estilos.

--- frontend/src/components/DetailCard.jsx
+++ frontend/src/components/DetailCard.jsx
@@ -14,11 +14,11 @@ const DetailCard = ({ product }) => {
         />
       </div>
       <div className='detail--product-text'>
-        <h3 className='tex-product'>{product.product_name} </h3>
-        <p>{product.short_description} <br />Descripción general: {product.long_description} <br />Lo puedes canjear por: {product.price} puntos.
+        <h3 className='nombre-product'>{product.product_name} </h3>
+        <p>{product.short_description} <br />Descripción: {product.long_description} <br />Lo puedes canjear por: {product.price} puntos.
         </p>
         <Link to="/Purchase">
-          <MDBBtn rounded className='mx-2' color='secondary'>CANJEAR</MDBBtn>
+          <MDBBtn rounded className='mx-2 btn-canjear' color='secondary'>CANJEAR</MDBBtn>
         </Link>
       </div>
     </div>
--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Navbar from './Navbar'
 import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
-import '../styles/gift.css'
 import Product from './Product';
 
 const Products = () => {
