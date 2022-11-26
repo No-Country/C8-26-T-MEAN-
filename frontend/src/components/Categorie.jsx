4de484bbf26db6bd58c3df71c7f7c89c94d30338
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-11-25 19:25:13 -0500

    REFACTOR: Se modificaron los estilos de las categorías. No se ocultan los elementos "a" cuando las imágenes haceb zoom.

--- frontend/src/components/Categorie.jsx
+++ frontend/src/components/Categorie.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
 import { Link } from 'react-router-dom';
-const Categorie = ({props}) => {
+import '../styles/categories.css'
+const Categorie = ({ props }) => {
   return (
     <MDBCol lg='2' md='2' className='mb-4 hover-zoom'>
-        <Link to='/Products' >
-    <img
-      src={props.image_url}
-      className='img-fluid rounded-circle'
-      alt=''
-      />
+      <Link to='/Products' >
+        <img
+          src={props.image_url}
+          className='img-fluid rounded-circle img_categ'
+          alt=''
+        />
       </Link>
-       <a><Link to='/Products' >{props.name}</Link></a>
-  </MDBCol>
+      <a className='a-product'><Link to='/Products' >{props.name}</Link></a>
+    </MDBCol>
   )
 }
 
