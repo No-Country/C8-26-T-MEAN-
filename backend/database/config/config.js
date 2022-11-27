dedbdb68ae1e6cc16b4bb110f1e8e8b017954dd0
Lucia Riva <85132067+LuciaRiva@users.noreply.github.com>
2022-11-27 14:29:57 -0300

    login

--- backend/database/config/config.js
+++ backend/database/config/config.js
@@ -10,7 +10,7 @@
 module.exports = {
     "development": {
         "username": "root",
-        "password": "nocountry",
+        "password": "1234",
         "database": "giftclub_db",
         "host": "127.0.0.1",
         "dialect": "mysql",
