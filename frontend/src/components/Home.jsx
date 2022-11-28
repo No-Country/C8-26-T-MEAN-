f5b620ff45f8b25e27cc474edc3b7fb96e12f133
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-11-28 14:44:39 -0500

    REFACTOR: Se dió añadió el container y contenido de categorías.

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,53 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 import Login from './Login'
 import Navbar from './Navbar'
 import CarouselOfi from './CarouselOfi'
-import Categories from './Categories'
-
+import Categorie from './Categorie'
 import '../styles/categories.css'
 import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
 
 const Home = () => {
+
+   const [categories, setCategories] = useState()
+   useEffect(() => {
+      const url = "http://localhost:3001/categories"
+      axios.get(url)
+         .then(res => setCategories(res.data.data))
+         .catch(e => { console.log(e) })
+   }, [])
+
    return (
       <div>
          <Navbar />
          <CarouselOfi />
-         {/* <Categories /> */}
-         {/* <MDBRow>
-            <MDBCol lg='2' md='2' className='mb-4 hover-zoom'>
-               <img
-                  src='https://wallpapercave.com/wp/wp7304269.jpg'
-                  className='img-fluid rounded-circle'
-                  alt=''
-               />
-               <a href='#/Products' class="a-product">AHORROS</a>
-            </MDBCol>
-            <MDBCol lg='2' md='2' className='mb-4 hover-zoom'>
-               <img
-                  src='https://wallpapercave.com/wp/wp7304269.jpg'
-                  className='img-fluid rounded-circle'
-                  alt=''
-               />
-               <a href='#/Products' class="a-product">  MASCOTAS  </a>
-            </MDBCol>
-            <MDBCol lg='2' md='2' className='mb-4 hover-zoom'>
-               <img
-                  src='https://wallpapercave.com/wp/wp7304269.jpg'
-                  className='img-fluid rounded-circle'
-                  alt=''
-               />
-               <a href='#/Products' class="a-product">INDUMENTARIA</a>
-            </MDBCol>
-            <MDBCol lg='2' md='2' className='mb-4 hover-zoom'>
-               <img
-                  src='https://wallpapercave.com/wp/wp7304269.jpg'
-                  className='img-fluid rounded-circle'
-                  alt=''
-               />
-               <a href='#/Products' class="a-product">TECNOLOGÍA</a>
-            </MDBCol>
-         </MDBRow> */}
-         <Login/>
+         <Login />
+         <div className='container'>
+            <MDBRow>
+               {
+                  categories?.map(categorie => (
+                     <Categorie props={categorie} key={categorie.id} />
+                  ))
+               }
+            </MDBRow>
+         </div>
       </div>
    );
 }
