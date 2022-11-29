31cf5be7fb9f6fe024722f6b34da305835c2076a
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-11-29 16:53:38 -0500

    REFACTOR: Modificación de respuestas.

--- frontend/src/components/Gift.jsx
+++ frontend/src/components/Gift.jsx
@@ -10,23 +10,25 @@ const Gift = () => {
       <Navbar />
       <div className='gift'>
         <img src={logo} alt="logo" class="img-preg" />
-          <MDBAccordion borderless initialActive={1}>
-            <MDBAccordionItem collapseId={1} headerTitle='¿Qué es GiftClub?'>
-              Es un programa gratuito de beneficios que permite acumular puntos para ser canjeados por premios dentro de un amplio catálogo. Este programa de fidelización permite sumar puntos cada vez que se realizan consumos con tarjeta de débito y crédito, según los siguientes criterios:
-              Tarjetas de Débito: 1 punto por cada 5 $/u$s.
-              Tarjetas de Crédito: 1 punto por cada 8 $/u$s.
-            </MDBAccordionItem>
-            <MDBAccordionItem collapseId={2} headerTitle='¿Qué tarjetas participan?'>
-              Las tarjetas Visa Débito, Visa Crédito, Maestro, Mastercard Crédito, American Express Crédito.
-            </MDBAccordionItem>
-            <MDBAccordionItem collapseId={3} headerTitle='¿Dónde puedo consultar mis puntos?'>
-              Podrás consultar tus puntos desde GITF CLUB ingresando con tu usuario y contraseña.
-            </MDBAccordionItem>
-            <MDBAccordionItem collapseId={4} headerTitle='¿Cómo puedo canjear mis puntos?'>
-              Al acceder al catálogo de productos, podrás elegir el que quieras adquirir y con tan sólo un click
-              en CANJEAR, te direccionará al carrito de compras para que puedas obtenerlo.
-            </MDBAccordionItem>
-          </MDBAccordion>
+        <MDBAccordion borderless initialActive={1}>
+          <MDBAccordionItem collapseId={1} headerTitle='¿Qué es GiftClub?'>
+            GiftClub es un sistema de puntos acumulables que pueden ser canjeados
+            por cualquiera de los productos que se encuentran dentro del catálogo de este sitio.<br />
+            Puedes sumar puntos cada vez que realizas consumos con tarjeta de débito o crédito, según los siguientes criterios:<br />
+            Tarjetas de Débito: 1 punto por cada 5 $/u$s.<br />
+            Tarjetas de Crédito: 1 punto por cada 8 $/u$s.
+          </MDBAccordionItem>
+          <MDBAccordionItem collapseId={2} headerTitle='¿Qué tarjetas participan?'>
+            Las tarjetas Visa Débito, Visa Crédito, Maestro, Mastercard Crédito, American Express Crédito.
+          </MDBAccordionItem>
+          <MDBAccordionItem collapseId={3} headerTitle='¿Dónde puedo consultar mis puntos?'>
+            Podrás consultar tus puntos desde GITF CLUB ingresando con tu usuario y contraseña.
+          </MDBAccordionItem>
+          <MDBAccordionItem collapseId={4} headerTitle='¿Cómo puedo canjear mis puntos?'>
+            Al acceder al catálogo de productos, podrás elegir el que quieras adquirir y con tan sólo un click
+            en CANJEAR, te direccionará al carrito de compras para que puedas obtenerlo.
+          </MDBAccordionItem>
+        </MDBAccordion>
       </div>
     </div>
   )
