6a1a878efdc1d1ef8c2e1f714e7e9fc03bf5822a
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-11-29 17:33:59 -0500

    REFACTOR: Se eliminó el párrafo que se encontraba debajo de título.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -92,12 +92,6 @@ function Login(){
 				   ¡Bienvenido a<br />
 				  <span className="text-primary">GIFTClub!</span>
 				</h1>
-				<p className='px-3'>
-				  ¿Qué es GiftClub? GiftClub es un sistema de puntos acumulables que pueden ser canjeados
-				  por cualquiera de los productos que se encuentran dentro del catálogo de este sitio. 
-				  Recuerda que a más puntos más posibilidades de llevarte todos los productos. (;
-				  <br />¡Que los disfrutes!
-				</p>
 			  </MDBCol>
 			  <MDBCol md='6'>
 				<MDBCard className='my-5'>
