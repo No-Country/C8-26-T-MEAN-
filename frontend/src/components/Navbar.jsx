76dddd43ae9d46f5e41c44b1d4e544716779de5d
FlorencioQuelca <florens.lonwe@gmail.com>
2022-12-02 14:55:18 -0400

    LOGIN LOGOUT TERMINADO

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -31,58 +31,49 @@ const Navbar= () =>{
     const local =sessionStorage.getItem('usuario');
   //  console.log(local,"1231")
     const usuario=JSON.parse(local)
-    //console.log(local2,"adsasdasd")
+    console.log(usuario,"adsasdasd")
         if(usuario){
- 
-
+          const address=usuario.address || "dasd"
+            const email=usuario.email
+          const id=usuario.id
+          const image=usuario.image
+          const name=usuario.name
+          const points=usuario.points
+          const role=usuario.role
           
-        } else{
-
-
-
+          if(usuario.orderSales!==0){
+             let orderPoints=usuario.orderSales.ammount
+             let cant=usuario.orderSales.items_q
+             dispatch(setValueProduct({cant}))
+            dispatch(setValue({id,name,email,points,role,address,orderPoints,cant,image}))
+          }else{
+            dispatch(setValueProduct({cant:0}))
+            dispatch(setValue({id,name,email,points,role,address,orderPoints:0,cant:0,image}))
+          }
+          setSession(true)
         }
-
   }, [])
-  
-
   //Código sacado del componente Login.jsx
   const privateSeed = 'DigitalHouse';
-
   const username = useRef();
   const password = useRef();
-  console.log(username);
-  const [loginError, setLoginError] = useState(false);
-  const [redirect, setRedirect] = useState(false);
+  const [session, setSession] = useState(false);
   const dispatch = useDispatch();
-  const notifySucces = () => toast("Ingreso correctamente");
-  const notifyError = (e) => toast(`Error al Ingresar: ${e}`);
-
+  const notifySucces = () => toast("Wellcome to GifClub");
+  const notifyError = (e) => toast(`Error: ${e}`);
   const loginFetch = (e) => {
     e.preventDefault();
-    setLoginError(false);
-    
     const cryptedPassword = crypto.AES.encrypt(password.current.value, privateSeed).toString();
     const data = {
       username: username.current.value,
       password: cryptedPassword,
-    }
-    console.log(data)
-
-    const registerLogin = data => {
-      if (data.access === 'Granted') {
-        sessionStorage.setItem('usuario', JSON.stringify(data.user));
-        setRedirect(true);
-      } else {
-        setLoginError(true);
-        sessionStorage.removeItem('usuario');
-      }
     }
     axios.post(`${BACKEND_ADDRESS}/users/auth`,data)
     .then(res =>
       {
         const data =res.data
         console.log(data.user)
-        registerLogin(data)
+        sessionStorage.setItem('usuario', JSON.stringify(data.user));
         const address=data.user.adress
         const email=data.user.email
         const id=data.user.id
@@ -90,68 +81,49 @@ const Navbar= () =>{
         const name=data.user.name
         const points=data.user.points
         const role=data.user.role
-        
-        if(data.orderSales){
+        if(data.orderSales!==0){
            let orderPoints=data.user.orderSales.ammount
            let cant=data.user.orderSales.items_q
            dispatch(setValueProduct({cant}))
           dispatch(setValue({id,name,email,points,role,address,orderPoints,cant,image}))
-
-
         }else{
           dispatch(setValueProduct({cant:0}))
           dispatch(setValue({id,name,email,points,role,address,orderPoints:0,cant:0,image}))
         }
-                // dispatch(setValuePoints(data.user.points))     
-                 notifySucces()
-         setBasicModal(!basicModal);
-         
+        // dispatch(setValuePoints(data.user.points))     
+        notifySucces()
+        setBasicModal(!basicModal);
+        setSession(true)
+        
       }).catch(e=>{
         console.log(e)
         notifyError(e.response.data.detail)
       });
-  }
-  //Fin
-
-  const useSessionStorage = (keyName, defaultValue) => {
-    const [storedValue, setStoredValue] = React.useState(() => {
-      try {
-        const value = window.sessionStorage.getItem(keyName);
-
-        if (value) {
-          return JSON.parse(value);
-        } else {
-          return defaultValue;
-        }
-      } catch (err) {
-        return defaultValue;
-      }
-    });
-
-    const setValue = newValue => {
-      try {
-        window.sessionStorage.setItem(keyName, JSON.stringify(newValue));
-      } catch (err) { }
-      setStoredValue(newValue);
-    };
-
-    return [storedValue, setValue];
-  }
-
-  const [user, setUser] = useSessionStorage('usuario', '');
+    }
+    
   const userLog = useSelector(state => state.user)
   const product = useSelector(state => state.product)
-  //const points= useSelector(state =>state.points)
-
 
   //Esto es para el modal:
   const navigate = useNavigate() 
   const [basicModal, setBasicModal] = useState(false);
-  const toggleShow = () => setBasicModal(!basicModal);
-const handleClick = () =>{
-navigate("/Purchase")
-}
+  const toggleShow = () =>{
+    
+     if(!session){
+       setBasicModal(!basicModal)
+     }else{
 
+      sessionStorage.removeItem('usuario');
+      dispatch(setValueProduct(""))
+      dispatch(setValue(""))
+      console.log("dasdasd")
+    
+    setSession(false)
+    }
+  } 
+const handleClick = () =>{
+     navigate("/Purchase")
+}
   return (
     <div>
       <div>
@@ -166,7 +138,6 @@ navigate("/Purchase")
             </MDBNavbarLink>
           </li>
           <li>
-
              <div className='logo'>
               <img src={userLog.image} alt="" />
              </div>
@@ -175,15 +146,20 @@ navigate("/Purchase")
                <div>
               {userLog.name}
             </div>
-            <div>
-              {userLog.points} Puntos
+            {
+              session? 
+              <div>
+                 {userLog.points} Puntos
+
             </div>
+              :
+              ""
+            }
           </li>
           <li>
             <MDBBtn onClick={toggleShow} size='lg' rounded className='mx-2' color='primary'>
-              Ingresar
+           { !session ? "ingresar": "salir"}
             </MDBBtn>
-
             <MDBModal show={basicModal} setShow={setBasicModal} tabIndex='-1'>
               <MDBModalDialog>
                 <MDBModalContent>
@@ -197,16 +173,13 @@ navigate("/Purchase")
                         <input type="text" ref={username} className="form-control" id="email"/>
                         <label htmlFor="">Contraseña:</label>
                         <input type="password" ref={password} className="form-control" id="password"/>
-	 				
-                   
-                    {redirect ? <Link to="/" /> : ''}
                     <div className="text-center">
                     
                       <p>¿Te olvidaste la contraseña?</p>
                   
                     </div>
                     <MDBBtn className="col-12 mb-2" onClick={loginFetch}>Ingresar</MDBBtn>
-                    {loginError ? <div className="alert alert-danger text-center my-4 fs-2">Usuario o contraseña incorrecta</div> : <></>}
+                
                   </MDBModalBody>
                  
                 </MDBModalContent>
