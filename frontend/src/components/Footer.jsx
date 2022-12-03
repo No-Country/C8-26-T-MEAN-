0dfcb9826dc31a9221dd29e3e5e3e84527a638e8
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-12-03 07:12:56 -0500

    EDIT: Cambios en el footer.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,134 +5,135 @@ import {
   MDBRow,
   MDBCol,
   MDBIcon,
-  MDBRipple
+  MDBRipple,
+  MDBBtn
 } from 'mdb-react-ui-kit';
 
 const Footer = () => {
   return (
     <div>
-      <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
-        <section className=''>
-          <MDBContainer className='p-4'>
-            <MDBRow className='mt-3'>
-              <MDBCol lg='3' md='12' className='mx-auto mb-4'>
-                <h6 className='text-uppercase'>GiftClub</h6>
-                <p>
-                  Es un sistema de puntos acumulables que pueden ser canjeados por cualquiera
-                  de los productos que se encuentran dentro del catálogo de este sitio. <br />
-                  A más puntos tenga el usuario, habrá màs posibilidad de llevarse todos los
-                  productos.
-                </p>
+      <MDBFooter className='text-center' color='white' bgColor='dark'>
+        <div className='p-4 text-center text-white d-flex justify-content-center'>
+          <section>
+            <MDBRow className='d-flex justify-content-center'>
+              <p className='fw-bold'>NUESTRO EQUIPO:</p>
+              <MDBCol lg='1' md='12' className='mb-4 mb-md-0'>
+                <MDBRipple
+                  rippleColor='light'
+                  className='bg-image hover-overlay shadow-1-strong rounded'
+                >
+                  <img src='https://mdbcdn.b-cdn.net/img/new/fluid/city/113.webp' className='w-100' />
+                  <a href='#!'>
+                    <div
+                      className='mask'
+                      style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
+                    ></div>
+                  </a>
+                </MDBRipple>
               </MDBCol>
-              <MDBCol lg='9' md='12' className='mx-auto mb-4'>
-                <h6 className='text-uppercase'>Conozca a nuestro equipo:</h6>
-                <MDBRow>
-                  <MDBCol lg='2' md='6' className='mb-4 mb-md-0'>
-                    <MDBRipple
-                      rippleColor='light'
-                      className='bg-image hover-overlay shadow-1-strong rounded'
-                    >
-                      <img src='https://mdbcdn.b-cdn.net/img/new/fluid/city/113.webp' className='w-100' />
-                      <a href='#!'>
-                        <div
-                          className='mask'
-                          style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
-                        ></div>
-                      </a>
-                    </MDBRipple>
-                  </MDBCol>
-                  <MDBCol lg='2' md='6' className='mb-4 mb-md-0'>
-                    <MDBRipple
-                      rippleColor='light'
-                      className='bg-image hover-overlay shadow-1-strong rounded'
-                    >
-                      <img src='https://mdbcdn.b-cdn.net/img/new/fluid/city/113.webp' className='w-100' />
-                      <a href='#!'>
-                        <div
-                          className='mask'
-                          style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
-                        ></div>
-                      </a>
-                    </MDBRipple>
-                  </MDBCol>
-                  <MDBCol lg='2' md='6' className='mb-4 mb-md-0'>
-                    <MDBRipple
-                      rippleColor='light'
-                      className='bg-image hover-overlay shadow-1-strong rounded'
-                    >
-                      <img src='https://mdbcdn.b-cdn.net/img/new/fluid/city/111.webp' className='w-100' />
-                      <a href='#!'>
-                        <div
-                          className='mask'
-                          style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
-                        ></div>
-                      </a>
-                    </MDBRipple>
-                  </MDBCol>
-                  <MDBCol lg='2' md='6' className='mb-4 mb-md-0'>
-                    <MDBRipple
-                      rippleColor='light'
-                      className='bg-image hover-overlay shadow-1-strong rounded'
-                    >
-                      <img src='https://mdbcdn.b-cdn.net/img/new/fluid/city/112.webp' className='w-100' />
-                      <a href='#!'>
-                        <div
-                          className='mask'
-                          style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
-                        ></div>
-                      </a>
-                    </MDBRipple>
-                  </MDBCol>
-                  <MDBCol lg='2' md='6' className='mb-4 mb-md-0'>
-                    <MDBRipple
-                      rippleColor='light'
-                      className='bg-image hover-overlay shadow-1-strong rounded'
-                    >
-                      <img src='https://mdbcdn.b-cdn.net/img/new/fluid/city/114.webp' className='w-100' />
-                      <a href='#!'>
-                        <div
-                          className='mask'
-                          style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
-                        ></div>
-                      </a>
-                    </MDBRipple>
-                  </MDBCol>
-                  <MDBCol lg='2' md='6' className='mb-4 mb-md-0'>
-                    <MDBRipple
-                      rippleColor='light'
-                      className='bg-image hover-overlay shadow-1-strong rounded'
-                    >
-                      <img src='https://mdbcdn.b-cdn.net/img/new/fluid/city/115.webp' className='w-100' />
-                      <a href='#!'>
-                        <div
-                          className='mask'
-                          style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
-                        ></div>
-                      </a>
-                    </MDBRipple>
-                  </MDBCol>
-                  <MDBCol lg='2' md='6' className='mb-4 mb-md-0'>
-                    <MDBRipple
-                      rippleColor='light'
-                      className='bg-image hover-overlay shadow-1-strong rounded'
-                    >
-                      <img src='https://mdbcdn.b-cdn.net/img/new/fluid/city/116.webp' className='w-100' />
-                      <a href='#!'>
-                        <div
-                          className='mask'
-                          style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
-                        ></div>
-                      </a>
-                    </MDBRipple>
-                  </MDBCol>
-                </MDBRow>
+              <MDBCol lg='1' md='12' className='mb-4 mb-md-0'>
+                <MDBRipple
+                  rippleColor='light'
+                  className='bg-image hover-overlay shadow-1-strong rounded'
+                >
+                  <img src='https://mdbcdn.b-cdn.net/img/new/fluid/city/111.webp' className='w-100' />
+                  <a href='#!'>
+                    <div
+                      className='mask'
+                      style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
+                    ></div>
+                  </a>
+                </MDBRipple>
+              </MDBCol>
+              <MDBCol lg='1' md='12' className='mb-4 mb-md-0'>
+                <MDBRipple
+                  rippleColor='light'
+                  className='bg-image hover-overlay shadow-1-strong rounded'
+                >
+                  <img src='https://mdbcdn.b-cdn.net/img/new/fluid/city/112.webp' className='w-100' />
+                  <a href='#!'>
+                    <div
+                      className='mask'
+                      style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
+                    ></div>
+                  </a>
+                </MDBRipple>
+              </MDBCol>
+              <MDBCol lg='1' md='12' className='mb-4 mb-md-0'>
+                <MDBRipple
+                  rippleColor='light'
+                  className='bg-image hover-overlay shadow-1-strong rounded'
+                >
+                  <img src='https://mdbcdn.b-cdn.net/img/new/fluid/city/114.webp' className='w-100' />
+                  <a href='#!'>
+                    <div
+                      className='mask'
+                      style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
+                    ></div>
+                  </a>
+                </MDBRipple>
+              </MDBCol>
+              <MDBCol lg='1' md='12' className='mb-4 mb-md-0'>
+                <MDBRipple
+                  rippleColor='light'
+                  className='bg-image hover-overlay shadow-1-strong rounded'
+                >
+                  <img src='https://mdbcdn.b-cdn.net/img/new/fluid/city/115.webp' className='w-100' />
+                  <a href='#!'>
+                    <div
+                      className='mask'
+                      style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
+                    ></div>
+                  </a>
+                </MDBRipple>
+              </MDBCol>
+              <MDBCol lg='1' md='12' className='mb-4 mb-md-0'>
+                <MDBRipple
+                  rippleColor='light'
+                  className='bg-image hover-overlay shadow-1-strong rounded'
+                >
+                  <img src='https://mdbcdn.b-cdn.net/img/new/fluid/city/116.webp' className='w-100' />
+                  <a href='#!'>
+                    <div
+                      className='mask'
+                      style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
+                    ></div>
+                  </a>
+                </MDBRipple>
+              </MDBCol>
+              <MDBCol lg='1' md='12' className='mb-4 mb-md-0'>
+                <MDBRipple
+                  rippleColor='light'
+                  className='bg-image hover-overlay shadow-1-strong rounded'
+                >
+                  <img src='https://mdbcdn.b-cdn.net/img/new/fluid/city/116.webp' className='w-100' />
+                  <a href='#!'>
+                    <div
+                      className='mask'
+                      style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
+                    ></div>
+                  </a>
+                </MDBRipple>
               </MDBCol>
             </MDBRow>
-          </MDBContainer>
-        </section>
-        <div className='text-center p-4 fw-bold' style={{ backgroundColor: 'rgba(254, 195, 16)' }}>
-          © 2022 |
-          <a className='fw-bold'> Un proyecto del equipo: C8-26-T-MEAN</a>
+          </section>
+        </div>
+
+        <MDBContainer className='p-4'>
+
+          <section className='mb-4 text-white'>
+            <p>
+              ¿Qué es GiftClub?<br />Es un sistema de puntos acumulables que pueden ser canjeados por cualquiera
+              de los productos que se encuentran dentro del catálogo de este sitio. <br />
+              A más puntos tenga el usuario, habrá más posibilidad de llevarse todos los
+              productos.
+            </p>
+          </section>
+        </MDBContainer>
+
+        <div className='text-center p-3 fw-bold' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+          © 2022 | Un proyecto de:
+          <a className='text-white fw-bold'> C8-26-T-MEAN</a>
         </div>
       </MDBFooter>
 
