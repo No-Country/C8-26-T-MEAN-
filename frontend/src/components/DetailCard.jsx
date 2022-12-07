beecd6ad9b322dc98d47121d943083c0b2af0bc5
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-12-07 15:21:31 -0500

    FIX: Código con formato.

--- frontend/src/components/DetailCard.jsx
+++ frontend/src/components/DetailCard.jsx
@@ -3,7 +3,6 @@ import { Link, Navigate, useNavigate } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
 import { useDispatch, useSelector } from 'react-redux'
 import { setValueProduct } from '../store/slices/products.slice'
-import {getUserThunk} from '../store/slices/users.slice'
 import axios from 'axios';
 import '../styles/detailProduct.css'
 import 'react-toastify/dist/ReactToastify.css';
@@ -18,8 +17,6 @@ import {
   MDBCol
 } from 'mdb-react-ui-kit';
 
-
-
 const DetailCard = ({ product }) => {
   const navigate = useNavigate()
   const user = useSelector(state => state.user)
@@ -28,77 +25,73 @@ const DetailCard = ({ product }) => {
   const notifySuccess = () => toast("Agregado al Carrito")
   const notifyError = (e) => toast(`Error al adicionar al carrito: ${e}`)
 
-const handleClick =()=>{
-   if(user){
-    
-    const url='http://localhost:3001/agregar';
-    let data={}
-     if(user.orderSales==0){
-      data={
-        user:{
-            id:user.id,
-            email:user.email,
-            name:user.name,
-            orderSales:user.orderSales,
-            points:user.points,
-            role:user.role,
-            address:user.address || "dasdasd"
-        },
-        product: {
-          id: product.id,
-          price: product.price
-        },
-     
-      }
-     }else{
-      data={
-        user:{
-            id:user.id,
-            email:user.email,
-            name:user.name,
-            points:user.points,
-            orderSales:user.orderSales,
-            address:user.address || "dasdasd"
+  const handleClick = () => {
+    if (user) {
+      const url = 'http://localhost:3001/agregar';
+      let data = {}
+      if (user.orderSales == 0) {
+        data = {
+          user: {
+            id: user.id,
+            email: user.email,
+            name: user.name,
+            orderSales: user.orderSales,
+            points: user.points,
+            role: user.role,
+            address: user.address || "dasdasd"
           },
           product: {
             id: product.id,
             price: product.price
           },
-         
+        }
+      } else {
+        data = {
+          user: {
+            id: user.id,
+            email: user.email,
+            name: user.name,
+            points: user.points,
+            orderSales: user.orderSales,
+            address: user.address || "dasdasd"
+          },
+          product: {
+            id: product.id,
+            price: product.price
+          },
+        }
       }
-
-     }
-     console.log(data)
-    axios.post(url, data)
-      .then(res => {
-        // const cant =
-          if(cant.cant){
-            dispatch(setValueProduct({cant:cant.cant+1}))
-          }else{
-            dispatch(setValueProduct({cant:1}))
+      console.log(data)
+      axios.post(url, data)
+        .then(res => {
+          // const cant =
+          if (cant.cant) {
+            dispatch(setValueProduct({ cant: cant.cant + 1 }))
+          } else {
+            dispatch(setValueProduct({ cant: 1 }))
 
           }
-    //  dispatch(getUserThunk(user))
-        console.log(res.data)
-        notifySuccess()
-        setTimeout(() => {
-          console.log("Delayed for 1 second.");
-          navigate("/Purchase")
-        }, "1000")
+          //  dispatch(getUserThunk(user))
+          console.log(res.data)
+          notifySuccess()
+          setTimeout(() => {
+            console.log("Delayed for 1 second.");
+            navigate("/Purchase")
+          }, "1000")
 
-    })
-    .catch(e=>{
-      console.log(e)
-      notifyError(e.response.data.message)
-    })
+        })
+        .catch(e => {
+          console.log(e)
+          notifyError(e.response.data.message)
+        })
 
-    }else{
-      
+    } else {
+
       notifyError("Debe ingresar con su cuenta para agregar el producto al Carrito")
-   }
+    }
 
-  
-}
+
+  }
   return (
     <div className='detail--product d-flex justify-content-center'>
 
