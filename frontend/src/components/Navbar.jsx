3db153946e6edded761ebd88f34a1c19285d0e31
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2022-12-07 23:13:26 -0500

    ADD: Logo para responsive, menú hamburguesa sin funcionalidad, nombre del usuario en negrita.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import React, { useState,useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { setValue } from '../store/slices/users.slice';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {getProductThunk,setValueProduct} from '../store/slices/products.slice'
-import {getUserThunk} from '../store/slices/users.slice'
+import { getProductThunk, setValueProduct } from '../store/slices/products.slice'
+import { getUserThunk } from '../store/slices/users.slice'
 import crypto from 'crypto-js'
 import axios from 'axios';
 import '../styles/navbar.css'
@@ -26,27 +26,27 @@ import {
 
 const BACKEND_ADDRESS = 'http://localhost:3001';
 
-const Navbar= () =>{
-   
+const Navbar = () => {
+
   const dispatch = useDispatch();
- useEffect(() => {
-    const local =sessionStorage.getItem('usuario');
-  //  console.log(local,"1231")
-    const usuario=JSON.parse(local)
-   // console.log(usuario,"adsasdasd")
-        if(usuario){
-          //  console.log(usuario)
-       //  dispatch(getUserThunk(usuario))
-          
-          setSession(true)
-        }
+  useEffect(() => {
+    const local = sessionStorage.getItem('usuario');
+    //  console.log(local,"1231")
+    const usuario = JSON.parse(local)
+    // console.log(usuario,"adsasdasd")
+    if (usuario) {
+      //  console.log(usuario)
+      //  dispatch(getUserThunk(usuario))
+
+      setSession(true)
+    }
   }, [])
   //Código sacado del componente Login.jsx
   const privateSeed = 'DigitalHouse';
   const username = useRef();
   const password = useRef();
   const [session, setSession] = useState(false);
-  const notifySucces = () => toast("Wellcome to GifClub");
+  const notifySucces = () => toast("Bienvenido a  GifClub, iniciaste sesión correctamente.");
   const notifyError = (e) => toast(`Error: ${e}`);
   const loginFetch = (e) => {
     e.preventDefault();
@@ -55,83 +55,83 @@ const Navbar= () =>{
       username: username.current.value,
       password: cryptedPassword,
     }
-    axios.post(`${BACKEND_ADDRESS}/users/auth`,data)
-    .then(res =>
-      {
-        const data =res.data
+    axios.post(`${BACKEND_ADDRESS}/users/auth`, data)
+      .then(res => {
+        const data = res.data
         console.log(data.user)
         sessionStorage.setItem('usuario', JSON.stringify(data.user));
-        const address=data.user.adress
-        const email=data.user.email
-        const id=data.user.id
-        const image=data.user.image
-        const name=data.user.name
-        const points=data.user.points
-        const role=data.user.role
-        const ordersalesofi=data.orderSales
-        if(ordersalesofi==0){
-          dispatch(setValueProduct({cant:0}))
-          dispatch(setValue({id,name,email,points,role,address,orderSales:0,cant:0,image}))
-        }else{
-          let orderSales=data.user.orderSales.ammount
-          let cant=data.user.orderSales.items_q
-          dispatch(setValueProduct({cant}))
-         dispatch(setValue({id,name,email,points,role,address,orderSales,cant,image}))
+        const address = data.user.adress
+        const email = data.user.email
+        const id = data.user.id
+        const image = data.user.image
+        const name = data.user.name
+        const points = data.user.points
+        const role = data.user.role
+        const ordersalesofi = data.orderSales
+        if (ordersalesofi == 0) {
+          dispatch(setValueProduct({ cant: 0 }))
+          dispatch(setValue({ id, name, email, points, role, address, orderSales: 0, cant: 0, image }))
+        } else {
+          let orderSales = data.user.orderSales.ammount
+          let cant = data.user.orderSales.items_q
+          dispatch(setValueProduct({ cant }))
+          dispatch(setValue({ id, name, email, points, role, address, orderSales, cant, image }))
         }
         // dispatch(setValuePoints(data.user.points))     
         notifySucces()
         setBasicModal(!basicModal);
         setSession(true)
-        
-      }).catch(e=>{
+
+      }).catch(e => {
         console.log(e)
         notifyError(e.response.data.detail)
       });
-    }
-    
+  }
+
   const userLog = useSelector(state => state.user)
   const product = useSelector(state => state.product)
 
   //Esto es para el modal:
-  const navigate = useNavigate() 
+  const navigate = useNavigate()
   const [basicModal, setBasicModal] = useState(false);
-  const toggleShow = () =>{
-    
-     if(!session){
-       setBasicModal(!basicModal)
-     }else{
+  const toggleShow = () => {
+
+    if (!session) {
+      setBasicModal(!basicModal)
+    } else {
 
       sessionStorage.removeItem('usuario');
-      dispatch(setValueProduct({cant:0}))
+      dispatch(setValueProduct({ cant: 0 }))
       dispatch(setValue(""))
-    //  console.log("dasdasd")
+      //  console.log("dasdasd")
       setSession(false)
     }
-  } 
-const handleClick = () =>{
-      navigate("/Purchase")
-}
+  }
+  const handleClick = () => {
+    navigate("/Purchase")
+  }
   return (
     <div>
       <div>
+        <MDBIcon icon='bars' fas className='menu-hamburguesa-resp' />
         <Link to='/' >
           <span className='gitclub-logo'></span>
         </Link>
         <ul className='lista-boton-carrito'>
-            <MDBBtn onClick={toggleShow} size='lg' rounded className='mx-2 btn-ingresar-responsive' color='primary'>
-            { !session ? "ingresar": "salir"}
-            </MDBBtn>
+          <MDBBtn onClick={toggleShow} size='lg' rounded className='mx-2 btn-ingresar-responsive' color='primary'>
+            {!session ? "ingresar" : "salir"}
+          </MDBBtn>
           <li id="icono_li">
             {
               session ?
                 <div>
-                <MDBNavbarLink id="icono" onClick={handleClick}>
-                  <MDBIcon fas icon='shopping-cart' />
+                  <MDBNavbarLink id="icono" onClick={handleClick}>
+                    <MDBIcon fas icon='shopping-cart' />
                     {product.cant}
-                </MDBNavbarLink>
+                  </MDBNavbarLink>
                 </div>
-              :
-              ""
+                :
+                ""
             }
           </li>
           <li>
@@ -140,17 +140,17 @@ const handleClick = () =>{
             </div>
           </li>
           <li>
-              <div>
+            <div className='user-name-info'>
               {userLog.name}
             </div>
             {
-              session? 
-              <div>
-                {userLog.points} Puntos
+              session ?
+                <div className='user-point-info'>
+                  {userLog.points} puntos
 
-            </div>
-              :
-              ""
+                </div>
+                :
+                ""
             }
           </li>
 
@@ -163,20 +163,20 @@ const handleClick = () =>{
                     <MDBBtn className='btn-close' color='none' onClick={toggleShow}></MDBBtn>
                   </MDBModalHeader>
                   <MDBModalBody className="form-group">
-                    
-                        <label htmlFor="">Nombre de usuario o email:</label>
-                        <input type="text" ref={username} className="form-control" id="email"/>
-                        <label htmlFor="">Contraseña:</label>
-                        <input type="password" ref={password} className="form-control" id="password"/>
+
+                    <label htmlFor="">Nombre de usuario o email:</label>
+                    <input type="text" ref={username} className="form-control" id="email" />
+                    <label htmlFor="">Contraseña:</label>
+                    <input type="password" ref={password} className="form-control" id="password" />
                     <div className="text-center">
-                    
+
                       <p>¿Te olvidaste la contraseña?</p>
-                  
+
                     </div>
                     <MDBBtn className="col-12 mb-2" onClick={loginFetch}>Ingresar</MDBBtn>
-                
+
                   </MDBModalBody>
-                 
+
                 </MDBModalContent>
               </MDBModalDialog>
             </MDBModal>
